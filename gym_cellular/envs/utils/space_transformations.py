e99b488a9c10ce1f86c26837c30164678d34299a
carlhenrikrolf <henrik.aaslund@gmail.com>
2023-03-22 17:07:58 +0000

    pruned cellular and tabular transformations

--- gym_cellular/envs/utils/space_transformations.py
+++ gym_cellular/envs/utils/space_transformations.py
@@ -21,3 +21,26 @@ def tabular2cellular(anint, intracellular_size, n_cells):
         alist[cell] = anint % intracellular_size
         anint = anint // intracellular_size
     return alist
+
+
+def pruned_cellular2tabular(alist, cellular_state_space):
+    
+    assert len(alist) == len(cellular_state_space)
+
+    n_cells = len(cellular_state_space)
+    anint = 0
+    place_value = 1
+    for cell in range(n_cells):
+        anint += (alist[cell] - min(cellular_state_space[cell])) * place_value
+        place_value *= len(cellular_state_space[cell])
+    return anint
+
+def pruned_tabular2cellular(anint, cellular_state_space):
+
+    n_cells = len(cellular_state_space)
+    alist = np.zeros(n_cells, dtype=int)
+    for cell in range(n_cells):
+        alist[cell] = anint % len(cellular_state_space[cell])
+        alist[cell] += min(cellular_state_space[cell])
+        anint = anint // len(cellular_state_space[cell])
+    return alist
